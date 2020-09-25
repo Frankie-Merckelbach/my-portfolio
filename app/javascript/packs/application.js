478f282856fa44f8a4e1e7d95396b46b4709a249
Frankie Merckelbach <frankiemerckelbach@gmail.com>
2020-09-25 14:38:14 +0200

    styling of navbar and buttons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,19 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// External imports
+import "bootstrap";
+import { initNavbar } from "../components/navbar"
+import {initScrollClick } from "../components/scrollclick"
+
+// Internal imports, e.g:
+// import { initSelect2 } from '../components/init_select2';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your functions here, e.g:
+  // initSelect2();
+  initNavbar();
+  initScrollClick();
+
+});
