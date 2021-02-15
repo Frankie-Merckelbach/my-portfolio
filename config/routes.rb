a4cc43e9c3226e0df0b5946f24467823793d7418
Frankie Merckelbach <frankiemerckelbach@gmail.com>
2021-02-15 15:20:34 +0100

    new links work with path

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/about', to: 'pages#about'
   get '/contact', to: 'pages#contact'
+  get '/experience', to: 'pages#experience'
+  get '/certification', to: 'pages#certification'
+  get '/skills', to: 'pages#skills'
+  # get '/interests', to: 'pages#experience'
+
 end
